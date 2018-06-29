b75d65d7f845ada14c2c8b608eb1a5c06f5f1f65
Duncan <frsdun@gmail.com>
2018-06-29 17:14:32 +0200

    Buttons and testimonial fade

---
+++ web/js/index.js
@@ -0,0 +1,19 @@
+// testimonial slide logic
+let slideIndex = 0;
+cycle_slides();
+
+function cycle_slides() {
+    let slide_array = document.getElementsByClassName("slide");
+
+    for (let i = 0; i < slide_array.length; i++) {
+        slide_array[i].classList.add("slide-off");
+        slide_array[i].classList.remove("slide-on");
+    }
+
+    slideIndex++;
+    if (slideIndex > slide_array.length) { slideIndex = 1 }
+    slide_array[slideIndex - 1].classList.remove("slide-off");
+    slide_array[slideIndex - 1].classList.add("slide-on");
+
+    setTimeout(cycle_slides, 6000); //time interval must match css
+}
