b41c771f7b18f87897159b863e55354446d54636
Duncan <frsdun@gmail.com>
2018-11-27 14:05:19 +0200

    Testing template structures

--- v3/build.py
+++ v3/build.py
@@ -11,10 +11,11 @@ jinja_env = Environment(
 )
 
 
-def render_template_to_file(template_path, output_folder, view_data):
+def render_template_to_file(template_path, view_data):
     template = jinja_env.get_template(template_path)
     rendered_html = template.render(view_data)
 
+    output_folder = 'web'
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
@@ -25,11 +26,39 @@ def render_template_to_file(template_path, output_folder, view_data):
 if __name__ == '__main__':
     # List of files to render.
     # Input files must be in the 'templates' folder (match jinja_env path)
-    render_template_to_file('t_index.html', 'web', dict(
+    render_template_to_file('index.html', dict(
+        title="VR Sketch | Design in virtual reality",
         nav_menu=[dict(name="Features", link="#features"),
                   dict(name="Pricing", link="#pricing"),
                   dict(name="Gallery", link="#gallery"),
-                  dict(name="Documentation", link="#"),
+                  dict(name="Learn", link="#learn"),
                   dict(name="Blog", link="#")]
     ))
+    render_template_to_file('knowledge.html', dict(
+        title="Knowledge Base | VR Sketch",
+        nav_menu=[dict(name="Documentation", link="documentation.html"),
+                  dict(name="FAQs", link="faqs.html"),
+                  dict(name="Tutorials", link="tutorials.html")]
+    ))
+    render_template_to_file('documentation.html', dict(
+        title="Documentation | VR Sketch",
+        nav_menu=[dict(name="Documentation", link="documentation.html"),
+                  dict(name="FAQs", link="faqs.html"),
+                  dict(name="Tutorials", link="tutorials.html")]
+    ))
+    render_template_to_file('tutorials.html', dict(
+        title="Tutorials | VR Sketch",
+        nav_menu=[dict(name="Documentation", link="documentation.html"),
+                  dict(name="FAQs", link="faqs.html"),
+                  dict(name="Tutorials", link="tutorials.html")]
+    ))
+    render_template_to_file('faqs.html', dict(
+        title="FAQs | VR Sketch",
+        nav_menu=[dict(name="Documentation", link="documentation.html"),
+                  dict(name="FAQs", link="faqs.html"),
+                  dict(name="Tutorials", link="tutorials.html")]
+    ))
+
+    render_template_to_file('requirements.html', dict(
+        title="Requirements | VR Sketch"))
     print("Build complete")
