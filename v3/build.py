2314ac52f3ad5e2eb5fae3a1ab4da9b37df98089
Duncan <frsdun@gmail.com>
2018-11-27 10:56:36 +0200

    templateing in-progress

--- v3/build.py
+++ v3/build.py
@@ -11,23 +11,25 @@ jinja_env = Environment(
 )
 
 
-def render_to_file(html_input_path, output_folder, view_data):
-    template = jinja_env.get_template(html_input_path)
+def render_template_to_file(template_path, output_folder, view_data):
+    template = jinja_env.get_template(template_path)
     rendered_html = template.render(view_data)
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    with open(output_folder + '\\' + html_input_path, "w") as output_file:
-        output_file.write(rendered_html)
+    with open(output_folder + '\\' + template_path, "w") as output_file:
+        output_file.write(rendered_html.encode('utf-8'))
 
 
 if __name__ == '__main__':
     # List of files to render.
     # Input files must be in the 'templates' folder (match jinja_env path)
-    render_to_file('t_index.html', 'web', dict(
-        name="Yip",
-        my_list=["one", "2"],
-        nav_menu=[dict(name="L1", link="#"),
-                  dict(name="L2", link="#")]
+    render_template_to_file('t_index.html', 'web', dict(
+        nav_menu=[dict(name="Features", link="#features"),
+                  dict(name="Pricing", link="#pricing"),
+                  dict(name="Gallery", link="#gallery"),
+                  dict(name="Documentation", link="#"),
+                  dict(name="Blog", link="#")]
     ))
+    print("Build complete")
