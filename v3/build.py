a479b7f07676ee395c15fa33cf1e9b835642aa50
Duncan <frsdun@gmail.com>
2018-11-27 15:20:54 +0200

    Better structure

--- v3/build.py
+++ v3/build.py
@@ -3,6 +3,7 @@
 import os
 from jinja2 import Environment, PackageLoader, select_autoescape
 import json
+import markdown
 
 # Set up Jinja2 environment
 jinja_env = Environment(
@@ -12,6 +13,7 @@ jinja_env = Environment(
 
 
 def render_template_to_file(template_path, view_data):
+    # Input files must be in the 'templates' folder (matching jinja_env path)
     template = jinja_env.get_template(template_path)
     rendered_html = template.render(view_data)
 
@@ -19,46 +21,41 @@ def render_template_to_file(template_path, view_data):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    with open(output_folder + '\\' + template_path, "w") as output_file:
-        output_file.write(rendered_html.encode('utf-8'))
+    # with open(output_folder + '\\' + template_path, "w") as output_file:
+    #     output_file.write(rendered_html.encode('utf-8'))
+    write_html_file(output_folder + '\\' + template_path, rendered_html)
+
+
+def html_from_markdown(markdown_path, output_path):
+    with open(markdown_path) as f:
+        html_data = markdown.Markdown().convert(f.read().decode('utf8'))
+    write_html_file(output_path, html_data)
+
+
+def write_html_file(output_path, html_data):
+    with open(output_path, "w") as output_file:
+        output_file.write(html_data.encode('utf-8'))
 
 
 if __name__ == '__main__':
-    # List of files to render.
-    # Input files must be in the 'templates' folder (match jinja_env path)
-    render_template_to_file('index.html', dict(
-        title="VR Sketch | Design in virtual reality",
-        nav_menu=[dict(name="Features", link="#features"),
-                  dict(name="Pricing", link="#pricing"),
-                  dict(name="Gallery", link="#gallery"),
-                  dict(name="Learn", link="#learn"),
-                  dict(name="Blog", link="#")]
-    ))
-    render_template_to_file('knowledge.html', dict(
-        title="Knowledge Base | VR Sketch",
-        nav_menu=[dict(name="Documentation", link="documentation.html"),
-                  dict(name="FAQs", link="faqs.html"),
-                  dict(name="Tutorials", link="tutorials.html")]
-    ))
-    render_template_to_file('documentation.html', dict(
-        title="Documentation | VR Sketch",
-        nav_menu=[dict(name="Documentation", link="documentation.html"),
-                  dict(name="FAQs", link="faqs.html"),
-                  dict(name="Tutorials", link="tutorials.html")]
-    ))
-    render_template_to_file('tutorials.html', dict(
-        title="Tutorials | VR Sketch",
-        nav_menu=[dict(name="Documentation", link="documentation.html"),
-                  dict(name="FAQs", link="faqs.html"),
-                  dict(name="Tutorials", link="tutorials.html")]
-    ))
-    render_template_to_file('faqs.html', dict(
-        title="FAQs | VR Sketch",
-        nav_menu=[dict(name="Documentation", link="documentation.html"),
-                  dict(name="FAQs", link="faqs.html"),
-                  dict(name="Tutorials", link="tutorials.html")]
-    ))
+    # 1. Convert all markdown to html
+    html_from_markdown('markdown/documentation.md',
+                       'templates/generated/docs-markdown.html')
 
-    render_template_to_file('requirements.html', dict(
-        title="Requirements | VR Sketch"))
+    # 2. Render all templates
+    render_template_to_file('index.html', dict(
+        title="VR Sketch | Design in virtual reality"))
+
+    render_template_to_file('documentation.html', dict(
+        title="Documentation | VR Sketch"))
+
+    render_template_to_file('tutorials.html', dict(
+        title="Tutorials | VR Sketch"))
+
+    render_template_to_file('faqs.html', dict(
+        title="FAQs | VR Sketch"))
+
+    render_template_to_file('blog.html', dict(
+        title="Blog | VR Sketch"))
+
     print("Build complete")
