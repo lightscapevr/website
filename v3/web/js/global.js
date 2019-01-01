1e6188ae1271681be757903a8f58feecebd248cb
Maciej Fijalkowski <fijall@gmail.com>
2019-01-01 14:11:56 +0100

    fix the hosts, wonder if we can't just use one

--- v3/web/js/global.js
+++ v3/web/js/global.js
@@ -1,4 +1,4 @@
-if (window.location.host == 'test.vrsketch.eu') {
+if (window.location.host == 'test.vrsketch.eu' || window.location.host == 'dev.vrsketch.eu') {
     GOOGLE_CLIENT_TOKEN_ID = '1076106158582-2hr4jav5kbn2gccs0jsdbdhr4sg1d399.apps.googleusercontent.com';
 } else {
     GOOGLE_CLIENT_TOKEN_ID = '1076106158582-vekr6opr52b6i8eeu3cc8si7828hisgj.apps.googleusercontent.com';
