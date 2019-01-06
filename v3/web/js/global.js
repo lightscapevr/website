fadf4e437d07767e1baf70756bef3b6e7d6bab0f
Maciej Fijalkowski <fijall@gmail.com>
2019-01-06 11:05:24 +0100

    add yet another host id, let's see if it works

--- v3/web/js/global.js
+++ v3/web/js/global.js
@@ -1,5 +1,7 @@
-if (window.location.host == 'test.vrsketch.eu' || window.location.host == 'dev.vrsketch.eu') {
+if (window.location.host == 'test.vrsketch.eu') {
     GOOGLE_CLIENT_TOKEN_ID = '1076106158582-2hr4jav5kbn2gccs0jsdbdhr4sg1d399.apps.googleusercontent.com';
+} else if (window.location.host = 'dev.vrsketch.eu') {
+    GOOGLE_CLIENT_TOKEN_ID = '1076106158582-vqlj400ho3b2dpeiqd2bvofu20mhsj46.apps.googleusercontent.com';    
 } else {
     GOOGLE_CLIENT_TOKEN_ID = '1076106158582-vekr6opr52b6i8eeu3cc8si7828hisgj.apps.googleusercontent.com';
 }
