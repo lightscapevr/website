57f6fa12afdb8ccece76a31cc0c454d13c1b6c56
Maciej Fijalkowski <fijall@gmail.com>
2022-03-09 09:13:51 +0200

    hide the modal too

--- web/js/index.js
+++ web/js/index.js
@@ -51,6 +51,7 @@ function getUserInfo() {
 function log_in_button() {
     // Only used until gapi.auth2 is loaded
     var btn = $("#login-button");
+    $("#login-type-modal").modal("hide");
     if (btn.html() == 'Log in') {
         btn.html('Log in&nbsp;<i class="fa fa-spin fa-spinner">');
         PENDING = function () {
