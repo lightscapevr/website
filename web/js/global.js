bf4add8f664fc95cd3d19612446f93bef88c37e4
Maciej Fijalkowski <fijall@gmail.com>
2023-03-30 14:29:42 +0200

    add parser

--- web/js/global.js
+++ web/js/global.js
@@ -25,9 +25,21 @@ let LOGIN_NAME_2 = "</button>";
 let RESET_PASSWORD_BUTTON = "&nbsp;&nbsp;<button type='button' class='btn btn-primary' "+
                             "onclick='password_reset(); return false;'>Reset password to the typed one</button>";
 
+
+function parseJwt (token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+}
+
+
 function handleCredentialResponse(r)
 {
-    console.log(r);
+    console.log(parseJwt(r.credential));
 }
 
 function show_error_message(errmsg) {
